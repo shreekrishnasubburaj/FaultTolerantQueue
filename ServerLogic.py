103feae921cff8509249611d56c06a92b3c63309
Shree Krishna Subburaj <shsu1807@colorado.edu>
2020-03-05 21:23:49 -0700

    FTQueue and server side code

--- ServerLogic.py
+++ ServerLogic.py
@@ -1,4 +1,6 @@
 import heapq
+import socket
+
 class Message(object):
     def __init__(self, GS: int, GSReq: int, mId: int, OP: int, clAddr: str, sendAddr: str):
         self.GS = GS
@@ -14,21 +16,74 @@ class Message(object):
     def __lt__(self, other):
         return self.GS < other.GS
 
+idCounter = 0
+FTQueue = {}  
+labelIdMap = {}
 buffer1 = []
 buffer2 = []
 buffer3 = []
-Addr = ""
-pid = 1
+HOST = ""
+PORT = 2
+pid = PORT
 GS = 0
 
+
+    
+def performOperation(msg):
+    opt = msg.OP[0]
+    if opt == 0: #qCreate
+        label = msg.OP[1]
+        if label in labelIdMap:
+            return labelIdMap[label]
+        else:
+            labelIdMap[label] = idCounter
+            FTQueue[idCounter] = []
+            idCounter+=1
+            return idCounter-1
+        
+    elif opt == 1: #qDestroy
+        id = msg.OP[1]
+        if id in FTQueue:
+            del FTQueue[id]
+        
+    elif opt == 2: #qId
+        label = msg.OP[1]
+        if label in labelIdMap:
+            return labelIdMap[label]
+        else:
+            return -1
+        
+    elif opt == 3: #qPush
+        id = msg.OP[1]
+        item = msg.OP[2]
+        if id in FTQueue:
+            FTQueue[id].append(item)
+        
+    elif opt == 4: #qPop
+        id = msg.OP[1]
+        if id in FTQueue:
+            if FTQueue[id]:
+                item = FTQueue[id].pop(0)
+                return item
+    elif opt == 5: #qTop
+        id = msg.OP[1]
+        if id in FTQueue:
+            if FTQueue[id]:
+                return FTQueue[id][0]
+        
+    elif opt == 6: #qSize
+        id = msg.OP[1]
+        if id in FTQueue:
+            return len(FTQueue[id])
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 while True:
     if buffer1:
         nextMsg = buffer1.pop()
         if nxtMsg.GS==-2: #NACK
             for msg in buffer2:
                 if msg.GS==nextMsg.GSReq:
-                    #send msg to requesting server
-                    print("a")
+                    server.sendto(msg, sendAddr) #send msg to requesting server
         elif nxtMsg.GS==-1: #Sequencer needs to set GS
             if nextMsg.mid%n==pid:
                 newMsg = Message(GS+1, nextMsg.mId, nextMsg.OP, nextMsg.clAddr, Addr)
